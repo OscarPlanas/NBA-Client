4d6b67d4a04ce33403b66cd4b989b17f7edb7f15
OscarPlanas <casplanas7@gmail.com>
2023-06-13 17:17:19 +0200

    Inicio de paillier

--- src/app/components/voting-page/voting-page.component.ts
+++ src/app/components/voting-page/voting-page.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import * as bc from 'bigint-conversion'
 import { SharedService } from 'src/app/data/hash';
+import axios from 'axios';
+import { MyPaillierPublicKey } from 'src/app/models/paillierPubKey';
+
 
 @Component({
   selector: 'app-voting-page',
@@ -12,6 +16,11 @@ export class VotingPageComponent {
   playerNames: string[] = [];
   playerRows!: string[][];
   responseData?: string;
+
+  pubKeyPaillierServerPromise: Promise<MyPaillierPublicKey>
+
+
+  
 //pubKeyServerPromise: Promise<MyRsaPublicKey>
   ngOnInit(): void {
     console.log("VotingPage");
@@ -27,17 +36,34 @@ export class VotingPageComponent {
   constructor(
     private route: ActivatedRoute,
     private sharedService: SharedService
-  ) {}
+  ) {
+    this.pubKeyPaillierServerPromise = this.getPaillierPubliKey();
+  }
   async initPlayers(): Promise<void> {
     const players = await this.getPlayers();
     this.playerNames = players.map((player: { name: string }) => player.name);
+
     this.playerRows = this.chunkArray(this.playerNames, 4);
+  }
+
+  /*server genera claves y la pública la pasa al cliente*/
+  getPaillierPubliKey = async (): Promise<MyPaillierPublicKey> => {
+    const res = await fetch('http://localhost:5432/api/paillier/getPailierPublicKey')
+    const key = await res.json()
+    console.log(key);
+    const pubKey = MyPaillierPublicKey.fromJSON(key)
+    console.log(pubKey);
+    return pubKey;
+    
+    
+   // return pubKey;
   }
 
   getPlayers = async (): Promise<{ name: string }[]> => {
     const response = await fetch('http://localhost:5432/api/players/');
     const players = await response.json();
     console.log(players.map((player: { name: string }) => player.name));
+
     return players;
   }
 
@@ -47,6 +73,14 @@ export class VotingPageComponent {
     console.log("HashUser to send with the vote " + hashUser);
     //const response = await fetch(`http://localhost:5432/api/players/${hashUser}/${playerName}`);
     console.log('Button clicked for player:', playerName);
+
+    const playerToPaillier = bc.textToBigint(playerName);
+    console.log("Player to paillier: " + playerToPaillier);
+    
+    const paillierPubKey = await this.pubKeyPaillierServerPromise;
+
+    const c1 = paillierPubKey.encrypt(playerToPaillier);
+    console.log("C1: " + c1);
    
   }
   private chunkArray(array: any[], size: number): any[][] {
